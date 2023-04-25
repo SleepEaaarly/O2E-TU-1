877b9e000960d4b82b8395c53b1615ff0b3f3922
Pentium <1793030808@qq.com>
2023-04-25 15:47:57 +0800

    fix: system chat submit, change tab to click

--- frontend/SE2023_Frontend_App-main/components/system-chat/system-chat-bottom.vue
+++ frontend/SE2023_Frontend_App-main/components/system-chat/system-chat-bottom.vue
@@ -7,7 +7,7 @@
          v-model="text" 
          confirm-type="search" 
          @confirm="submit"/>
-        <view class="icon iconfont icon-fabu u-f-ajc" @tab="submit"></view>
+        <view class="icon iconfont icon-fabu u-f-ajc" @click.native="submit"></view>
     </view>
 
 </template>
