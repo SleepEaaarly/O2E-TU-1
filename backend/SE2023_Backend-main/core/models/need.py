74a4fd6050783f55a8f71a91510dc48ac0aed248
LaiAng80686 <jipeng@gmail.com>
2023-04-25 15:24:38 +0800

    fix chatgpt report bugs 1524

--- backend/SE2023_Backend-main/core/models/need.py
+++ backend/SE2023_Backend-main/core/models/need.py
@@ -27,31 +27,43 @@ EMERGENCY_CODE = (
 
 
 class Need(models.Model):
-    #需求标题
+    NEED_FIELD_CHOICES = (
+        (0, '信息技术'),
+        (1, '装备制造'),
+        (2, '新材料'),
+        (3, '新能源'),
+        (4, '节能环保'),
+        (5, '生物医药'),
+        (6, '科学创意'),
+        (7, '检验检测'),
+        (8, '其他')
+    )
+    # 需求标题
     title = models.CharField(max_length=100)
-    #需求描述
+    # 需求描述
     description = models.TextField()
-    #预计经费（报酬）
+    # 预计经费（报酬）
     money = models.IntegerField()
-    #需求创建时间，自动填充
+    # 需求创建时间，自动填充
     start_time = models.CharField(max_length=30)
-    #有效时间（预计结束时间）
-    end_time = models.CharField(max_length=30,blank=True, null=True)
-    #关键字
+    # 有效时间（预计结束时间）
+    end_time = models.CharField(max_length=30, blank=True, null=True)
+    # 关键字
     key_word = models.CharField(max_length=200)
-    #技术领域
+    # 技术领域
     field = models.IntegerField(choices=NEED_FIELD_CHOICES, default=8)
-    #地址
+    # 地址
     address = models.CharField(max_length=200)
-    #需求公司
-    enterprise = models.ForeignKey(User, on_delete=models.CASCADE, related_name="enterprise_need")
-    #订单状态
+    # 需求公司
+    enterprise = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="enterprise_need")
+    # 订单状态
     state = models.IntegerField(choices=NEED_STATE, default=0)
-    #紧急程度
+    # 紧急程度
     emergency = models.IntegerField(choices=EMERGENCY_CODE, default=0)
-    #预计招募人数
+    # 预计招募人数
     predict = models.IntegerField()
-    #实际已招募人数
+    # 实际已招募人数
     real = models.IntegerField(default=0)
 
     vector_sci = models.TextField(blank=True, null=True)
