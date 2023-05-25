142b13fe4427685a84d3578f147fce4e10cbb591
iszry <iszry@foxmail.com>
2023-05-25 09:34:45 +0800

    [Bug review @ zry] add: Bug review in README.md

--- frontend/SE2023_Frontend_App-main/pages/platform/platform.vue
+++ frontend/SE2023_Frontend_App-main/pages/platform/platform.vue
@@ -57,7 +57,7 @@
 					<uni-nav-bar>
 						<block slot="left">
 							<view class="left-head">
-								<view style="margin-left: 40%;">
+								<view style="margin-left: 20%;">
 									<text class="uni-nav-bar-text" @click="showNeedType">{{field_items[field]}}</text>
 								</view>
 							</view>
